d42e673c764a624786a86270a591ef706c5689e3
Rob Ruifrok <robruifrok@multiweb.nl>
2019-11-04 09:30:25 +0100

    Update fdyn-script.js

--- fdyn-script.js
+++ fdyn-script.js
@@ -1,59 +1,63 @@
 jQuery( document ).ready(function($) {	
 
-	$('#primary').on('change', '.fdyn-filter-select', function() {		
+	//hide the EA-plugin select boxes
+	$('.filter.form-control').hide();
+	$('.ea-label.control-label').hide();
+	
+	$(document).on('change', '.eaf-filter-select', function() {		
 		
-		let fdworkerName = '';
-		let fdlocationName = '';
-		let fdserviceName = '';		
-		let fdduration = '';
-		let fdslotStep = '';
+		let eafworkerName = '';
+		let eaflocationName = '';
+		let eafserviceName = '';		
+		let eafduration = '';
+		let eafslotStep = '';
 		
-		let fdworker = $('#fd-worker').val();
-		let fdlocation = $('#fd-location').val();
-		let fdservice = $('#fd-service').val();
+		let eafworker = $('#eaf-worker').val();
+		let eaflocation = $('#eaf-location').val();
+		let eafservice = $('#eaf-service').val();
 		
 		$('.ajax-loader').addClass('active');
-		$('.fdyn-filter-select').prop('disabled', true).css('opacity', '0.5');
+		$('.eaf-filter-select').prop('disabled', true).css('opacity', '0.5');
 
 		
 		$.ajax({
-			url : eafilter.ajax_url,
+			url : eaffilter.ajax_url,
 			type : 'get',
 			data : {
-				action : 'ea_filter',
-				_ajax_nonce : eafilter.nonce,
-				fdworker: fdworker,
-				fdlocation: fdlocation,
-				fdservice: fdservice,
+				action : 'eaf_filter',
+				_ajax_nonce : eaffilter.nonce,
+				eafworker: eafworker,
+				eaflocation: eaflocation,
+				eafservice: eafservice,
 			},
 			success : function( response ) {
 				
 				//replace filter with reduced set of options
-				$('#fdyn-ea-filter' ).replaceWith(response);
+				$('#eaf-ea-filter' ).replaceWith(response);
 				
 				//worker, location and service can be selected because there was only one left
-				fdworker = $('#fd-worker').val();
-				fdlocation = $('#fd-location').val();
-				fdservice = $('#fd-service').val();
+				eafworker = $('#eaf-worker').val();
+				eaflocation = $('#eaf-location').val();
+				eafservice = $('#eaf-service').val();
 				
 				//replace content of original EA-plugin select boxes
-				if(fdlocation > 0) {
-					fdlocationName = $('#fd-location option[value=' + fdlocation + ']').text();
-					$('select[name=location]').html('<option selected="selected" value=' + fdlocation + '>'+fdlocationName+'</option>');					
+				if(eaflocation > 0) {
+					eaflocationName = $('#eaf-location option[value=' + eaflocation + ']').text();
+					$('select[name=location]').html('<option selected="selected" value=' + eaflocation + '>'+eaflocationName+'</option>');					
 				};
-				if(fdworker > 0) {
-					fdworkerName = $('#fd-worker option[value=' + fdworker + ']').text();
-					$('select[name=worker]').html('<option selected="selected" value=' + fdworker + '>'+fdworkerName+'</option>');					
+				if(eafworker > 0) {
+					eafworkerName = $('#eaf-worker option[value=' + eafworker + ']').text();
+					$('select[name=worker]').html('<option selected="selected" value=' + eafworker + '>'+eafworkerName+'</option>');					
 				};
-				if(fdservice > 0) {
-					fdserviceName = $('#fd-service option[value=' + fdservice + ']').text();
-					fdduration = $('#fd-service option[value=' + fdservice + ']').attr('data-duration');
-					fdslotStep = $('#fd-service option[value=' + fdservice + ']').attr('data-slot-step');
-					$('select[name=service]').html('<option  data-duration="' + fdduration + '" data-slot-step="' + fdslotStep + '" selected="selected" value=' + fdservice + '>'+fdserviceName+'</option>');
+				if(eafservice > 0) {
+					eafserviceName = $('#eaf-service option[value=' + eafservice + ']').text();
+					eafduration = $('#eaf-service option[value=' + eafservice + ']').attr('data-duration');
+					eafslotStep = $('#eaf-service option[value=' + eafservice + ']').attr('data-slot-step');
+					$('select[name=service]').html('<option  data-duration="' + eafduration + '" data-slot-step="' + eafslotStep + '" selected="selected" value=' + eafservice + '>'+eafserviceName+'</option>');
 				};								
-
-				//When worker, location and service are selected trigger chenge event on worker to start next step
-				if (fdworker > 0 && fdlocation > 0 && fdservice > 0) {					
+				
+				//When worker, location and service are selected trigger change event on worker to start next step
+				if (eafworker > 0 && eaflocation > 0 && eafservice > 0) {					
 					$('select[name=worker]').change();
 				};
 				
@@ -62,30 +66,33 @@ jQuery( document ).ready(function($) {
 	});
 	
 	//New reset button to reset custom filter
-	$('.ea-actions-group').append('<button class="btn btn-default ea-btn fd-ea-reset">Opnieuw</button>');
+	$('.ea-actions-group').append('<button class="btn btn-default ea-btn eaf-ea-reset">Opnieuw</button>');
+	$('.ea-cancel').hide();
 	
 	//Reset options in the custom filter
-	$('#primary').on('click', '.fd-ea-reset', function(event) {
+	$('#primary').on('click', '.eaf-ea-reset', function(event) {
 		event.preventDefault();
 		
-		let fdworker = '';
-		let fdlocation = '';
-		let fdservice = '';		
+		$('.ajax-loader').addClass('active');
+		let eafworker = '';
+		let eaflocation = '';
+		let eafservice = '';		
 		
 		$.ajax({
 			url : eafilter.ajax_url,
 			type : 'get',
 			data : {
-				action : 'ea_filter',
-				_ajax_nonce : eafilter.nonce,
-				fdworker: fdworker,
-				fdlocation: fdlocation,
-				fdservice: fdservice,
+				action : 'eaf_filter',
+				_ajax_nonce : eaffilter.nonce,
+				eafworker: eafworker,
+				eaflocation: eaflocation,
+				eafservice: eafservice,
 			},
 			success : function( response ) {
 				
 				//replace filter with complete set of options
-				$('#fdyn-ea-filter' ).replaceWith(response);
+				$('#eaf-ea-filter' ).replaceWith(response);
+								
 				//trigger click event on original cancel button
 				$('.ea-cancel').click();
 			}
@@ -95,11 +102,12 @@ jQuery( document ).ready(function($) {
 	
 	//Hide the new reset button when form is submitted
 	$(document).ajaxStop(function() {
-		if($('.ea-submit:hidden').length > 0) {
+		if($('.ea-submit:hidden').length > 0) {			
+			$('.eaf-ea-reset').hide();
+
 			
-			$('.fd-ea-reset').hide();
+			
 		};
 	});
-		
 
 });
